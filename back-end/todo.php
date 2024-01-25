d32f7e0646e90d40ba1812c1ca30ae713bb0a1bd
jacopobiagi <jacopo.biagi02@gmail.com>
2024-01-25 19:22:54 +0100

    feat: visualizzazione oggetto json

--- back-end/todo.php
+++ back-end/todo.php
@@ -1,6 +1,8 @@
 <?php
 
     header('Content-type: application/json');
+    header("Access-Control-Allow-Headers: X-Requested-With");
+    header("Access-Control-Allow-Origin: http://localhost:5173");
 
     $todo = [
         [
---
+++ front-end/src/components/AppMain.vue
@@ -0,0 +1,27 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      todoList: [],
+    };
+  },
+  methods: {
+    getTodo() {
+      axios
+        .get(
+          "http://localhost/eserciziBoolean/esercizi-php/php-todo-list-json/back-end/todo.php"
+        )
+        .then((res) => {
+          console.log(res.data);
+        });
+    },
+  },
+  mounted() {
+    this.getTodo();
+  },
+};
+</script>
+<template>sssssssss</template>
+<style></style>
